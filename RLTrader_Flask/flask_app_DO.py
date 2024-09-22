1c5536308b990408a7b6c5026e62f31b48fd0fb8
cyrus123live <64929788+cyrus123live@users.noreply.github.com>
2024-09-21 17:34:20 -0700

    add flask_app_DO

---
+++ RLTrader_Flask/flask_app_DO.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template, session, redirect, url_for
+import os
+from dotenv import load_dotenv
+import sqlite3 as sql
+# from flaskr.auth import login_required
+import datetime
+
+app = Flask(__name__)
+load_dotenv()
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
+@app.route('/')
+def index():
+    if 'name' in session and session['name'] == "admin":
+
+        conn = sql.connect("/root/RLTrader/RLTrader.db")
+        values = [[c[0] * c[2] + c[1], datetime.datetime.fromtimestamp(c[3]).day] for i, c in enumerate(conn.execute("SELECT close, cash, held, timestamp FROM trades").fetchall())]
+
+        return render_template("index.html", name=session['name'], data=[v[0] for v in values], labels=[v[1] for v in values])
+    return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+         
+        if request.form['Password'] == "password":
+            session['name'] = 'admin'
+        else:
+            return render_template("login.html")
+
+        return redirect(url_for('index'))
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('name', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
