cf681a71485acadb7dcd2e7bcbd3d2be1778f8df
cyrus123live <64929788+cyrus123live@users.noreply.github.com>
2024-10-21 16:19:52 -0700

    trying out 3x per min trades

--- run_trader_DO.py
+++ run_trader_DO.py
@@ -154,7 +154,7 @@ def main():
         if current_time.hour < 8:
             continue
 
-        if current_time.second == 20 or current_time.second == 40 or current_time.second == 0: 
+        if current_time.second == 20 or current_time.second == 50: 
             try:
                 data = StockData.get_current_data()
             except Exception as e:
@@ -192,7 +192,7 @@ def main():
             else:
                 print(f"{current_time.strftime('%Y-%m-%d %H:%M')} Holding at price {round(data['Close'].iloc[-1], 2)}")
 
-            time.sleep(10)
+            time.sleep(30)
             cancel_output = cancel_all()
             if len(cancel_output) > 0: # cancel orders if not made in 25 seconds, so that we can get up to date info and safely move to next minute
                 missed_trades += 1 
